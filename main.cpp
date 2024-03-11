9fe27a29b6fc804976f78c07a4c258cc91bd862c
kj-best <ckj123789@126.com>
2024-03-11 11:19:01 +0800

    创建项目

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+/*
+1.网站的404攻击
+2.网站篡改攻击
+3.网站的攻击记录
+4.dns攻击
+5.服务器重启攻击
+*/
+
+int main(void)
+{
+	std::cout << "1.网站的404攻击" << std::endl;
+	std::cout << "2.网站篡改攻击" << std::endl;
+	std::cout << "3.网站的攻击记录" << std::endl;
+	std::cout << "4.dns攻击" << std::endl;
+	std::cout << "5.服务器重启攻击" << std::endl;
+
+	system("pause");
+	return 0;
+
+
+}
